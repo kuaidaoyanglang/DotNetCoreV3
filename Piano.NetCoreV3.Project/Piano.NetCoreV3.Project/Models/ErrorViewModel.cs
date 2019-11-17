85510c888ee831defb2e723965e811c57647e843
yang.wei <1042744766@qq.com>
2019-11-17 16:37:26 +0800

    Init

---
+++ Piano.NetCoreV3.Project/Piano.NetCoreV3.Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Piano.NetCoreV3.Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Piano.NetCoreV3.Project/Piano.NetCoreV3.Project/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Piano.NetCoreV3.Project
+{
+    public class Program
+    {
+        /// <summary>
+        /// 为什么可以跨平台？
+        /// 因为微软出了可以在linux运行的clr
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
